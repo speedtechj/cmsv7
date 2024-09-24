53f83bcd3afa83e0b3e4edcdf2bd714b58eae460
speedtechj <arbiasjr@gmail.com>
2024-09-24 13:47:43 -0600

    update

--- app/Models/logsetting.php
+++ app/Models/logsetting.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class logsetting extends Model
+class Logsetting extends Model
 {
     use HasFactory;
     protected $guarded = [];
