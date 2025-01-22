09f2aac84ff76ae4e5d7dae75669b4cc523ceff9
speedtechj <arbiasjr@gmail.com>
2025-01-22 15:31:45 -0700

    update

--- database/migrations/2025_01_22_221317_add_picture_to_senders_table.php
+++ database/migrations/2025_01_22_221317_add_picture_to_senders_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('senders', function (Blueprint $table) {
-            $table->longText('picture')->nullable();
+            $table->longText('sender_picture')->nullable();
         });
     }
 
