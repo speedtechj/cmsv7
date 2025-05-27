0ff0bd43c21ebdefcc980850ce89723beb61d28d
speedtechj <arbiasjr@gmail.com>
2025-05-27 09:45:29 -0600

    update

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -207,7 +207,7 @@ class UserResource extends Resource
     public static function getRelations(): array
     {
         return [
-            ActivitylogRelationManager::class,
+            // ActivitylogRelationManager::class,
         ];
     }
 
