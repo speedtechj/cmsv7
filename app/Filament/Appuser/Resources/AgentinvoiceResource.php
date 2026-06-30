e57d7287fc0031d79f9ebc93a3dbeed9b3c9ab07
speedtechj <arbiasjr@gmail.com>
2026-06-30 15:30:56 -0600

    updaet

--- app/Filament/Appuser/Resources/AgentinvoiceResource.php
+++ app/Filament/Appuser/Resources/AgentinvoiceResource.php
@@ -39,7 +39,7 @@ class AgentinvoiceResource extends Resource
     {
         return $table
             ->defaultGroup('agent.full_name')
-             ->paginated([10, 25, 50, 1000, 'all'])
+             ->paginated([10, 25, 50, 200, 500])
             ->columns([
                 Tables\Columns\TextColumn::make('agent.full_name')
                     ->sortable(),
