a20df26ef47fff5a4d334c904a3f60529495226e
speedtechj <arbiasjr@gmail.com>
2026-09-23 12:14:33 -0600

    update

--- app/Filament/Appuser/Resources/ManifestResource.php
+++ app/Filament/Appuser/Resources/ManifestResource.php
@@ -47,16 +47,8 @@ class ManifestResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('booking_invoice')
                 ->label('Invoice')
-                ->searchable(query: function (Builder $query, string $search): Builder {
-        return $query->where(function (Builder $q) use ($search) {
-            $q->where('booking_invoice', 'like', "%{$search}%")
-              ->orWhereIn('sender_id', function ($sub) use ($search) {
-                  $sub->select('sender_id')
-                      ->from('bookings') // change to your table name
-                      ->where('booking_invoice', 'like', "%{$search}%");
-              });
-        });
-    })        ->sortable(),
+                ->searchable()
+                ->sortable(),
             Tables\Columns\TextColumn::make('manual_invoice')
                 ->label('Manual Invoice')
                 ->searchable()
