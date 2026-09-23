9b30edd827eb47eb5e8d621b459964e37b219211
speedtechj <arbiasjr@gmail.com>
2026-09-23 12:30:03 -0600

    update

--- app/Filament/Appuser/Resources/ManifestResource.php
+++ app/Filament/Appuser/Resources/ManifestResource.php
@@ -43,7 +43,7 @@ class ManifestResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
-         //   ->searchDebounce('750ms')
+            ->searchDebounce('750ms')
             ->columns([
                 Tables\Columns\TextColumn::make('booking_invoice')
                 ->label('Invoice')
